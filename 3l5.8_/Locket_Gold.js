1009a98e698aa2fdd97e27370bfd3a71fed7df4a
NgDanhThanhTrung <thanhtrungnguyendanh@gmail.com>
2026-01-09 18:58:03 +0600

    Update Locket_Gold.js

--- 3l5.8_/Locket_Gold.js
+++ 3l5.8_/Locket_Gold.js
@@ -1,42 +1,48 @@
-// ========= ID ========= //
 const mapping = {
-  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
-  'Locket': ['Gold']
+  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip', 'watch_vip'],
+  'Locket': ['Gold', 'com.is3l5.8.premium.yearly']
 };
 
-// =========  Phần cố định  ========= // 
-var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);
+const ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
+let obj = JSON.parse($response.body);
 
-obj.Attention="Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
+obj.subscriber = obj.subscriber || {};
+obj.subscriber.entitlements = obj.subscriber.entitlements || {};
+obj.subscriber.subscriptions = obj.subscriber.subscriptions || {};
 
-var is3l5.8={
-  is_sandbox:!1,
-  ownership_type:"PURCHASED",
-  billing_issues_detected_at:null,
-  period_type:"normal",
-  expires_date:"2999-12-18T01:04:17Z",
-  grace_period_expires_date:null,
-  unsubscribe_detected_at:null,
-  original_purchase_date:"2008-05-31T01:04:18Z",
-  purchase_date:"2008-05-31T01:04:17Z",
-  store:"app_store"
+const premiumInfo = {
+  is_sandbox: false,
+  ownership_type: "PURCHASED",
+  billing_issues_detected_at: null,
+  period_type: "normal",
+  expires_date: "2999-12-18T01:04:17Z",
+  original_purchase_date: "2008-05-31T01:04:17Z",
+  purchase_date: "2008-05-31T01:04:17Z",
+  store: "app_store"
 };
 
-var is3l5.8_sub={
-  grace_period_expires_date:null,
-  purchase_date:"2008-05-31T01:04:17Z",
-  product_identifier:"com.t22092002.premium.yearly",
-  expires_date:"2999-12-18T01:04:17Z"
+const entitlementInfo = {
+  grace_period_expires_date: null,
+  purchase_date: "2008-05-31T01:04:17Z",
+  product_identifier: "com.is3l5.8.premium.yearly",
+  expires_date: "2999-12-18T01:04:17Z"
 };
 
-const match=Object.keys(mapping).find(e=>ua.includes(e));
+const match = Object.keys(mapping).find(e => ua.includes(e));
 
-if(match){
-  let[e,s]=mapping[match];
-  s?(is3l5.8_sub.product_identifier=s,obj.subscriber.subscriptions[s]=is3l5.8):obj.subscriber.subscriptions["com.is3l5.8.premium.yearly"]=t22092002,obj.subscriber.entitlements[e]=is3l5.8_sub
-}else{
-  obj.subscriber.subscriptions["com.is3l5.8.premium.yearly"]=is3l5.8;
-  obj.subscriber.entitlements.pro=is3l5.8_sub
+if (match) {
+  let [entKey, subKey] = mapping[match];
+  let finalSubKey = subKey || "com.is3l5.8.premium.yearly";
+  
+  entitlementInfo.product_identifier = finalSubKey;
+  obj.subscriber.subscriptions[finalSubKey] = premiumInfo;
+  obj.subscriber.entitlements[entKey] = entitlementInfo;
+} else {
+  obj.subscriber.subscriptions["com.is3l5.8.premium.yearly"] = premiumInfo;
+  obj.subscriber.entitlements["Gold"] = entitlementInfo;
+  obj.subscriber.entitlements["pro"] = entitlementInfo;
 }
 
-$done({body:JSON.stringify(obj)});
+obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
+
+$done({ body: JSON.stringify(obj) });
