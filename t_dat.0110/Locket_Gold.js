5022abc964ad52147d2a49171dbb74aa37562781
NgDanhThanhTrung <thanhtrungnguyendanh@gmail.com>
2026-01-25 20:42:42 +0700

    Update Locket_Gold.js

--- t_dat.0110/Locket_Gold.js
+++ t_dat.0110/Locket_Gold.js
@@ -1,35 +1,48 @@
-// ========= ID ========= //
 const mapping = {
-  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
-  'Locket': ['Gold']
+  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip', 'watch_vip'],
+  'Locket': ['Gold', 'com.t_dat.0110.premium.yearly']
 };
-var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],
-obj=JSON.parse($response.body);
-obj.Attention="Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
-var t_dat.0110={
-  is_sandbox:!1,
-  ownership_type:"PURCHASED",
-  billing_issues_detected_at:null,
-  period_type:"normal",
-  expires_date:"2999-12-18T01:04:17Z",
-  grace_period_expires_date:null,
-  unsubscribe_detected_at:null,
-  original_purchase_date:"2024-10-01T01:04:18Z",
-  purchase_date:"2024-10-01T01:04:17Z",
-  store:"app_store"
+
+const ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
+let obj = JSON.parse($response.body);
+
+obj.subscriber = obj.subscriber || {};
+obj.subscriber.entitlements = obj.subscriber.entitlements || {};
+obj.subscriber.subscriptions = obj.subscriber.subscriptions || {};
+
+const premiumInfo = {
+  is_sandbox: false,
+  ownership_type: "PURCHASED",
+  billing_issues_detected_at: null,
+  period_type: "normal",
+  expires_date: "2999-12-18T01:04:17Z",
+  original_purchase_date: "2024-10-01T01:04:17Z",
+  purchase_date: "2024-10-01T01:04:17Z",
+  store: "app_store"
 };
-var t_dat.0110_sub={
-  grace_period_expires_date:null,
-  purchase_date:"2024-10-01T01:04:17Z",
-  product_identifier:"com.t_dat.0110.premium.yearly",
-  expires_date:"2999-12-18T01:04:17Z"
+
+const entitlementInfo = {
+  grace_period_expires_date: null,
+  purchase_date: "2024-10-10T01:04:17Z",
+  product_identifier: "com.t_dat.0110.premium.yearly",
+  expires_date: "2999-12-18T01:04:17Z"
 };
-const match=Object.keys(mapping).find(e=>ua.includes(e));
-if(match){
-  let[e,s]=mapping[match];
-  s?(t_dat.0110_sub.product_identifier=s,obj.subscriber.subscriptions[s]=t_dat.0110):obj.subscriber.subscriptions["com.t_dat.0110.premium.yearly"]=t_dat.0110,obj.subscriber.entitlements[e]=t_dat.0110_sub
-}else{
-  obj.subscriber.subscriptions["com.t_dat.0110.premium.yearly"]=t_dat.0110;
-  obj.subscriber.entitlements.pro=t_dat.0110_sub
+
+const match = Object.keys(mapping).find(e => ua.includes(e));
+
+if (match) {
+  let [entKey, subKey] = mapping[match];
+  let finalSubKey = subKey || "com.t_dat.0110.premium.yearly";
+  
+  entitlementInfo.product_identifier = finalSubKey;
+  obj.subscriber.subscriptions[finalSubKey] = premiumInfo;
+  obj.subscriber.entitlements[entKey] = entitlementInfo;
+} else {
+  obj.subscriber.subscriptions["com.t_dat.0110.premium.yearly"] = premiumInfo;
+  obj.subscriber.entitlements["Gold"] = entitlementInfo;
+  obj.subscriber.entitlements["pro"] = entitlementInfo;
 }
-$done({body:JSON.stringify(obj)});
+
+obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
+
+$done({ body: JSON.stringify(obj) });
